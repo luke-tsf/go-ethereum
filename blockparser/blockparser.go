73a83b714a5c2b4e578e2ccaa67986b16bcd71ec
Unknown <hungnmq@blockchainlabs.asia>
2018-09-12 17:35:12 +0700

    Link DB and EVM, Store and Query DB

--- blockparser/blockparser.go
+++ blockparser/blockparser.go
@@ -5,27 +5,42 @@ package blockparser
 import(
 	"fmt"
 	// "github.com/ethereum/go-ethereum/common"
-	"encoding/hex"
+	// "encoding/hex"
 )
 
 func (evmLogDb *EVMLogDb) GetNewEVMLog(evmLog *EVMLog) (bool){
-	sender := evmLog.sender.String()
-	senderHex, err := hex.DecodeString(sender)
+	batch := evmLogDb.customDb.NewBatch()
+	var key = evmLog.sender.String()
+	var value = string(evmLog.receiver.String())
+	batch.Put([]byte(key), []byte(value))
+	batch.Write()
+	getValue, err := evmLogDb.customDb.Get([]byte(key))
 	if err != nil{
 		return false
 	}
-	receiver := evmLog.receiver.String()
-	receiverHex, err := hex.DecodeString(receiver)
-	if err != nil{
-		return false
-	}
-	// value := evmLog.value.String()
-	evmLogDb.customDb.Put(senderHex,receiverHex)
-	result,err := evmLogDb.customDb.Get(senderHex)
-	if err != nil{
-		return false
-	}
-	fmt.Println(result)
+	fmt.Println(string(getValue))
+	// fmt.Println(evmLog)
+	// // sender := evmLog.sender.String()
+	// sender := evmLog.sender
+	// senderHex, err := hex.DecodeString(sender)
+	// fmt.Println(senderHex)
+	// fmt.Println(err)
+	// if err != nil{
+	// 	return false
+	// }
+	// // receiver := evmLog.receiver.String()
+	// receiver := evmLog.receiver
+	// receiverHex, err := hex.DecodeString(receiver)
+	// if err != nil{
+	// 	return false
+	// }
+	// // value := evmLog.value.String()
+	// evmLogDb.customDb.Put(senderHex,receiverHex)
+	// result,err := evmLogDb.customDb.Get(senderHex)
+	// if err != nil{
+	// 	return false
+	// }
+	// fmt.Println(result)
 	return true
 }
 
